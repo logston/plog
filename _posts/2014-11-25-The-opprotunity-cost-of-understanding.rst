723ba4c2126e87fcef224a00288a235250a48a6e
Paul Logston <code@logston.me>
2014-11-30 00:30:50 -0500

    Update deploy script.

---
+++ _posts/2014-11-25-The-opprotunity-cost-of-understanding.rst
@@ -0,0 +1,6 @@
+---
+layout: post
+title: The Oprotunity cost of understanding.
+---
+
+
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
 
-rm -rf /srv/plog/*
-cp -R $WORKSPACE/* /srv/plog/
+rm -rf /srv/plog/plog_src/*
+cp -R $WORKSPACE/* /srv/plog/plog_src/
+. /srv/plog/bin/activate
+cd /srv/plog/plog_src
+jekyll build
