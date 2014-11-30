e961021e8726dbadf534125ddb843ccff813785a
Paul Logston <code@logston.me>
2014-11-30 16:39:38 -0500

    Update deploy script.

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ rm -rf /srv/plog/plog_src/*
 cp -R $WORKSPACE/* /srv/plog/plog_src/
 . /srv/plog/bin/activate
 cd /srv/plog/plog_src
+git submodule add https://github.com/xdissent/jekyll-rst.git _plugins/jekyll-rst
 jekyll build
